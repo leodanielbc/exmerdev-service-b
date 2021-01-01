c78a6b7318fae81f31fa17eca2d8d155273e495a
Leonardo Benitez <benitez.leonardo06@gmail.com>
2021-01-01 14:26:40 -0500

    config inicial proyecto

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = 5100
+
+  // Swagger
+  const options = new DocumentBuilder()
+    .setTitle('Exmerdev')
+    .setDescription('Servicio Web A')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
+}
+bootstrap();
